f271f4e3614515df40729af39d4094fb5528109f
AshwiniGatty <ashgatty888@gmail.com>
2021-12-12 16:41:26 +0400

    Changes to check for build errors. Take-1

--- admin/config.tsx
+++ admin/config.tsx
@@ -1,12 +1,13 @@
 // admin/config.tsx
 /** @jsxRuntime classic */
 /** @jsx jsx */
+import { AdminConfig } from '@keystone-6/core/types';
 import { jsx } from '@keystone-ui/core';
 import CustomIcon from "../image/Icon.png";
 
 function CustomLogo () {
     return <img css = {{display: "inline-block", width: "250px", height: "auto"}} src = {CustomIcon.src} alt = "Image not found"/>
 }
-export const components = {
+export const components: AdminConfig['components'] = {
     Logo: CustomLogo
 }
