d6792ec7ea091d2562dd1695b8666726b8752d2d
AshwiniGatty <ashgatty888@gmail.com>
2021-12-12 17:32:46 +0400

    Resolve build erros. Take-2

--- admin/config.tsx
+++ admin/config.tsx
@@ -3,10 +3,11 @@
 /** @jsx jsx */
 import { AdminConfig } from '@keystone-6/core/types';
 import { jsx } from '@keystone-ui/core';
-import CustomIcon from "../image/Icon.png";
+// import CustomIcon from "../image/Icon.png";
 
 function CustomLogo () {
-    return <img css = {{display: "inline-block", width: "250px", height: "auto"}} src = {CustomIcon.src} alt = "Image not found"/>
+    // return <img css = {{display: "inline-block", width: "250px", height: "auto"}} src = {CustomIcon.src} alt = "Image not found"/>
+    return <h3> Chacra Software </h3>
 }
 export const components: AdminConfig['components'] = {
     Logo: CustomLogo
